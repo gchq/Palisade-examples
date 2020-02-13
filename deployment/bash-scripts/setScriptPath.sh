e5ef7c682b1dc1e9631efdf0d17ea6ac87078d4e
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-02-13 12:11:18 +0000

    Pal 231 updates based on latest PR comments

--- deployment/bash-scripts/setScriptPath.sh
+++ deployment/bash-scripts/setScriptPath.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # sets up the different paths for calling deployment scripts
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-EXAMPLE=$(pwd)
 
-export EXAMPLE="$EXAMPLE"
+export EXAMPLE=$(pwd)
 export K8SBASHSCRIPTS="$EXAMPLE/local-k8s/bash-scripts"
 export LOCALJVMBASHSCRIPTS="$EXAMPLE/local-jvm/bash-scripts"
 export GENERICSCRIPTS="$EXAMPLE/bash-scripts"
--- deployment/local-jvm/bash-scripts/runServiceLauncher.sh
+++ deployment/local-jvm/bash-scripts/runServiceLauncher.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #Start all the services using the service launcher from Palisade-services
-(cd ../Palisade-services && java -jar -Dspring.profiles.active=jvm services-launcher/target/services-launcher-0.4.0-SNAPSHOT-exec.jar)
+(cd ../Palisade-services && java -jar -Dspring.profiles.active=jvm services-launcher/target/services-launcher-*-exec.jar)
