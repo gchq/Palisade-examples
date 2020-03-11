1f7b7d1a588b60e3f944bba5f0fc0d97c85e6de1
Dev 930018 <dev930018@gmail.com>
2020-03-11 16:18:57 +0000

    PAL-546 added slf4j logging

--- hr-data-generator/src/main/java/uk/gov/gchq/palisade/example/hrdatagenerator/CreateData.java
+++ hr-data-generator/src/main/java/uk/gov/gchq/palisade/example/hrdatagenerator/CreateData.java
@@ -16,6 +16,9 @@
 
 package uk.gov.gchq.palisade.example.hrdatagenerator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import uk.gov.gchq.palisade.Util;
 
 import java.io.File;
@@ -26,13 +29,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public final class CreateData {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateData.class);
 
     private CreateData() {
     }
 
     public static void main(final String... args) {
         if (args.length < 3) {
-            System.out.println("This method needs at least three arguments. The directory path to save the files in, the number of employee's to generate and the number of files to split those employees between. An optional 4th argument is the number of threads to use which will default to 1.");
+            LOGGER.warn("This method needs at least three arguments. The directory path to save the files in, the number of employee's to generate and the number of files to split those employees between. An optional 4th argument is the number of threads to use which will default to 1.");
         } else {
             String outputFilePath = args[0];
             long numberOfEmployees = Long.parseLong(args[1]);
@@ -54,10 +58,10 @@ public final class CreateData {
                     response.get();
                 }
             } catch (final Exception e) {
-                System.err.println(e.getLocalizedMessage());
+                LOGGER.error(e.getLocalizedMessage());
             }
             long endTime = System.currentTimeMillis();
-            System.out.println("Took " + (endTime - startTime) + "ms to create " + numberOfEmployees + " employees");
+            LOGGER.info("Took " + (endTime - startTime) + "ms to create " + numberOfEmployees + " employees");
         }
     }
 }
