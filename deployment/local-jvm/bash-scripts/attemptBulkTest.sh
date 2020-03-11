32a35c514693c7c258456c14df13bcecc16151cc
Dev 930018 <dev930018@gmail.com>
2020-03-11 12:06:07 +0000

    PAL-546 restored bulk-example to reading a directory of files rather than a single one

--- deployment/local-jvm/bash-scripts/attemptBulkTest.sh
+++ deployment/local-jvm/bash-scripts/attemptBulkTest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run the bulk resource test
-java -jar example-model/target/example-model-*-exec.jar --example.directory="$(pwd)/resources/data/employee_file0.avro" --example.type=bulk || echo "Error running example-model-<version>-exec.jar - have you run 'mvn install'?"
+java -jar example-model/target/example-model-*-exec.jar --example.directory="$(pwd)/resources/data" --example.type=bulk || echo "Error running example-model-<version>-exec.jar - have you run 'mvn install'?"
