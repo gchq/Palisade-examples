ee43ddbcbe6731177118d25c6e4b5ad90a9392be
Dev 930018 <dev930018@gmail.com>
2020-03-18 11:36:38 +0000

    PAL-546 accounted for what counts as the current path presented to a process

--- deployment/local-jvm/bash-scripts/startServices.sh
+++ deployment/local-jvm/bash-scripts/startServices.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+cd ../Palisade-services/
 
-
-FILE=../Palisade-services/services-manager/target/services-manager-*-exec.jar || echo "Cannot find services-manager-<version>-exec.jar - have you run 'mvn install' in Palisade-services?"
+FILE=services-manager/target/services-manager-*-exec.jar || echo "Cannot find services-manager-<version>-exec.jar - have you run 'mvn install' in Palisade-services?"
 
 # Start all the services using the service manager from Palisade-services
 java -jar -Dspring.profiles.active=discovery $FILE --run
