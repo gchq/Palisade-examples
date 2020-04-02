e21e7f6ff428e0f78d1e24cf0e5ba4559003b6c6
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-04-02 09:50:35 +0100

    Pal 394 updated method accessors

--- example-library/src/main/java/uk/gov/gchq/palisade/example/config/ExamplePolicyCacheWarmerFactory.java
+++ example-library/src/main/java/uk/gov/gchq/palisade/example/config/ExamplePolicyCacheWarmerFactory.java
@@ -206,7 +206,7 @@ public class ExamplePolicyCacheWarmerFactory implements PolicyCacheWarmerFactory
         }
     }
 
-    public ParentResource getParent(final String fileURL) {
+    private ParentResource getParent(final String fileURL) {
         URI normalised = ExampleFileUtil.convertToFileURI(fileURL);
         //this should only be applied to URLs that start with 'file://' not other types of URL
         if (normalised.getScheme().equals(FileSystems.getDefault().provider().getScheme())) {
@@ -228,7 +228,7 @@ public class ExamplePolicyCacheWarmerFactory implements PolicyCacheWarmerFactory
         }
     }
 
-    public boolean isDirectoryRoot(final Path path) {
+    private boolean isDirectoryRoot(final Path path) {
         return StreamSupport
                 .stream(FileSystems.getDefault()
                         .getRootDirectories()
