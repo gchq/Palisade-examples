f4cc18ea72d10541de2eedb32b6304614374a5c1
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-04-06 09:50:03 +0100

    updated bean name

--- deployment/bash-scripts/waitForHost.sh
+++ deployment/bash-scripts/waitForHost.sh
@@ -8,9 +8,9 @@ shift
 cmd="$@"
 
 until $(curl --output /dev/null -sf $host); do
-  >&2 echo "$host is unavailable - sleeping"
+  echo >&2 "$host is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "$host is up - executing command"
-exec $cmd
+echo >&2 "$host is up - executing command"
+exec $cmdw
