40ca3113595c82cd41037dce27918a7178944a1f
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-04-27 12:28:19 +0100

    reverted verify

--- deployment/local-jvm/bash-scripts/verify.sh
+++ deployment/local-jvm/bash-scripts/verify.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-num=$(wc -l deployment/local-jvm/bash-scripts/exampleOutput.txt | awk '{ print $1 }')
-echo "$num"
+
+FILE=rest-example.log
+DIR=../Palisade-services/
+
+if [ -d $DIR ]; then
+  # Important to cd before running the jar - the working directory must be somewhere under Palisade-services
+  cd $DIR
+  # Start all the services using the service manager from Palisade-services
+  if [ -f $FILE ]; then
+    num=$(wc -l deployment/local-jvm/bash-scripts/$FILE | awk '{ print $1 }')
+    echo "$num"
+  else
+    echo "Cannot find rest-example.log, have you run the example?"
+  fi
+else
+  echo "Cannot find Palisade-services directory - have you run 'git clone'?"
+fi
