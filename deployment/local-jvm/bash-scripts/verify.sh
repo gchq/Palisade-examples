0889df6b12f61b3640cb7c407d2216e300e4f76c
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-05-01 12:51:46 +0100

    Revert "updated verify"
    This reverts commit fe076173

--- deployment/local-jvm/bash-scripts/verify.sh
+++ deployment/local-jvm/bash-scripts/verify.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-FILE=exampleOutput.txt
-DIR=deployment/local-jvm/bash-scripts/
+FILE=rest-example.log
+DIR=../Palisade-services/
 
 if [ -d $DIR ]; then
-  # Important to cd before running the jar - the working directory must be somewhere under Palisade-examples
+  # Important to cd before running the jar - the working directory must be somewhere under Palisade-services
   cd $DIR
   # Start all the services using the service manager from Palisade-services
   if [ -f $FILE ]; then
     num=$(wc -l $FILE | awk '{ print $1 }')
     echo "$num"
   else
-    echo "Cannot find exampleOutput.txt, have you run the example?"
+    echo "Cannot find rest-example.log, have you run the example?"
   fi
 else
-  echo "Cannot find Palisade-examples/deployment/local-jvm/bash-scripts/ directory - have you run 'git clone'?"
+  echo "Cannot find Palisade-services directory - have you run 'git clone'?"
 fi
