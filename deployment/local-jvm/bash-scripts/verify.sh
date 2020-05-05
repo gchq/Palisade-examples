caa1ca5a13755cef64cda390627604d2edd9f713
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-05-05 12:39:52 +0100

    Updated verify!

--- deployment/local-jvm/bash-scripts/verify.sh
+++ deployment/local-jvm/bash-scripts/verify.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-FILE=rest-example.log
-DIR=../Palisade-services/
+FILE=exampleOutput.txt
+DIR=deployment/local-jvm/bash-scripts/
 
 if [ -d $DIR ]; then
   # Important to cd before running the jar - the working directory must be somewhere under Palisade-services
   cd $DIR
-  # Start all the services using the service manager from Palisade-services
+
   if [ -f $FILE ]; then
     num=$(wc -l $FILE | awk '{ print $1 }')
     echo "$num"
   else
-    echo "Cannot find rest-example.log, have you run the example?"
+    echo "Cannot find exampleOutput.txt, have you run the example?"
   fi
 else
-  echo "Cannot find Palisade-services directory - have you run 'git clone'?"
+  echo "Cannot find deployment/local-jvm/bash-scripts/ - have you run 'git clone'?"
 fi
