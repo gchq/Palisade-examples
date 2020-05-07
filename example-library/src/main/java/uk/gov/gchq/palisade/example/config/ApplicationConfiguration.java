3edf6e6298cb57c0fc58d026c27902ebf3b04937
Dev 930018 <dev930018@gmail.com>
2020-05-07 17:01:05 +0100

    PAL-639 added resource builder to policies similar to users

--- example-library/src/main/java/uk/gov/gchq/palisade/example/config/ApplicationConfiguration.java
+++ example-library/src/main/java/uk/gov/gchq/palisade/example/config/ApplicationConfiguration.java
@@ -25,9 +25,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfiguration.class);
-
     @Bean
     @ConditionalOnProperty(prefix = "population", name = "userProvider", havingValue = "example")
     @ConfigurationProperties(prefix = "population")
@@ -40,18 +37,4 @@ public class ApplicationConfiguration {
     public ExampleUserPrepopulationFactory userPrepopulationFactory() {
         return new ExampleUserPrepopulationFactory();
     }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "population", name = "policyProvider", havingValue = "example")
-    @ConfigurationProperties(prefix = "population")
-    public ExamplePolicyConfiguration policyConfiguration() {
-        return new ExamplePolicyConfiguration();
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "population", name = "policyProvider", havingValue = "example")
-    public ExamplePolicyPrepopulationFactory policyPrepopulationFactory() {
-        return new ExamplePolicyPrepopulationFactory();
-    }
-
 }
