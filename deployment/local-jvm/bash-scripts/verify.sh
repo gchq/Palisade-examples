60dd791b6bb0e95b5a0c5c4577ef8fce66298115
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-05-18 12:24:35 +0100

    Pal 642 updated verify.sh pass/fail line count

--- deployment/local-jvm/bash-scripts/verify.sh
+++ deployment/local-jvm/bash-scripts/verify.sh
@@ -12,9 +12,9 @@ if [ -d $DIR ]; then
 
     #check the length of the exampleOutput text file and pass if its 980
     if [ $num == 944 ]; then
-      echo "Success - Number of lines was 184"
+      echo "Success - Number of lines was 183"
     else
-      echo "Number of lines was not 184 but was: $num"
+      echo "Number of lines was not 183 but was: $num"
       exit 1
     fi
 
