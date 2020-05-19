453ee7391da41f0e95eede3608af85cd381114dc
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-05-19 11:09:09 +0100

    Pal 642 updated run script to start using services-manager, created stopServices script

--- deployment/local-jvm/bash-scripts/stopServices.sh
+++ deployment/local-jvm/bash-scripts/stopServices.sh
@@ -3,13 +3,13 @@
 FILE=services-manager/target/services-manager-*-exec.jar
 DIR=../Palisade-services/
 
-# Run the rest example
 if [ -d $DIR ]; then
   # Important to cd before running the jar - the working directory must be somewhere under Palisade-services
   cd $DIR
-  # Start all the services using the service manager from Palisade-services
+  # Stop all the services using the service manager from Palisade-services
   if [ -f $FILE ]; then
-    java -jar -Dspring.profiles.active=rest $FILE --manager.mode=run
+    java -jar -Dspring.profiles.active=example,debug $FILE --manager.mode=shutdown
+    java -jar -Dspring.profiles.active=discovery $FILE --manager.mode=shutdown
   else
     echo "Cannot find services-manager-<version>-exec.jar - have you run 'mvn install' in Palisade-services?"
   fi
