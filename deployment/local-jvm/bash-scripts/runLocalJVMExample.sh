7bea39ef374491097ef3a3a2d4007923dab7f65a
Dev 930018 <dev930018@gmail.com>
2020-06-09 11:45:16 +0100

    PAL-655 renamed constants TO_UPPERCASE_SNAKECASE

--- deployment/local-jvm/bash-scripts/runLocalJVMExample.sh
+++ deployment/local-jvm/bash-scripts/runLocalJVMExample.sh
@@ -4,7 +4,7 @@ FILE=example-model/target/example-model-*-exec.jar
 
 # Run the rest example
 if [ -f $FILE ]; then
-  java -jar $FILE --example.filename="$(pwd)/resources/data" --example.type=rest
+  java -Dspring.profiles.active=eureka,rest -jar $FILE
 else
   echo "Cannot find example-model-<version>-exec.jar - have you run 'mvn install'?"
 fi
