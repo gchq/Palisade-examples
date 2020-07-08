698d460b942c020ae7dc98d557ae802fb55fa784
Dev 930018 <dev930018@users.noreply.github.com>
2020-07-08 16:17:24 +0100

    PAL-543 pwd

--- deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
+++ deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 helm dep up
-helm upgrade --install palisade .
+helm upgrade --install palisade . --set global.persistence.dataStore.palisade-data-store.local.hostPath=$(pwd)
