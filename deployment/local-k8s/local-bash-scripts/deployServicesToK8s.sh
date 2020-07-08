55028e038c36bf85bbecfd245c0d201e34711b67
Dev 930018 <dev930018@users.noreply.github.com>
2020-07-08 17:23:43 +0100

    PAL-543 typo'd dataStores plural

--- deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
+++ deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 helm dep up
-helm upgrade --install palisade . --set global.persistence.dataStore.palisade-data-store.local.hostPath=$(pwd)
+helm upgrade --install palisade . --set global.persistence.dataStores.palisade-data-store.local.hostPath=$(pwd)
