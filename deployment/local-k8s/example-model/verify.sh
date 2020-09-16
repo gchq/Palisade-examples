50d2e8fa8a3ea9658d04afc2c477d02c3fe61d7d
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-09-16 11:17:35 +0100

    added namespace to verify

--- deployment/local-k8s/example-model/verify.sh
+++ deployment/local-k8s/example-model/verify.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-kubectl exec $(kubectl get pods | awk '/example-model/ {print $1}') -- bash -c "cd /usr/share/example-model && bash ./verify.sh"
+kubectl exec $(kubectl get pods --namespace-pal-544 | awk '/example-model/ {print $1}') -- bash -c "cd /usr/share/example-model && bash ./verify.sh"
