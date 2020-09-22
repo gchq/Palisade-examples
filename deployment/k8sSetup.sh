64b58ed3ff2a528e36f58c912b62410e261657fb
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-09-22 13:04:31 +0100

    Pal 544 updated Dockerfile and setup script

--- deployment/k8sSetup.sh
+++ deployment/k8sSetup.sh
@@ -15,7 +15,9 @@
 
 val=$(kubectl get ns $1)
 if [ -z ${val} ]; then
+  echo "Deleting current palisade deployment on namespace ${val}"
   helm delete palisade -n ${val}
+  echo "Sleeping for 2 minutes"
   sleep 120
 fi
 
@@ -24,8 +26,11 @@ if [ $# -eq 1 ]; then
 
   mkdir -p /usr/share/deployment/classpath/example
   cp -r /usr/share/example-jars /usr/share/deployment/classpath/example
+  echo "Copied example-jars to /usr/share/deployment/classpath/example"
   cp -r /usr/share/example-data /data/local-data-store
+  echo "Copied example-data to /data/local-data-store"
   kubectl delete pods -n ${NAMESPACE} --all
+  echo "Restarted pods for namespace ${NAMESPACE}"
 else
   echo "1 argument should be passed"
 fi
