3f6a8967843b18ea51aff1621c0eb3abdbbae996
dev930018 <dev930018@gmail.com>
2021-01-14 14:13:19 +0000

    PAL-424 working end-to-end test using PAL-867-ResourceService-Auditing resource-service

--- deployment/aws-k8s/example-model/copyExampleData.sh
+++ deployment/aws-k8s/example-model/copyExampleData.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 mkdir -p /usr/share/deployment/classpath/example
-cp -r /usr/share/example-jars /usr/share/deployment/classpath/example
+cp -r /usr/share/example-jars/* /usr/share/deployment/classpath/example/
 echo "Copied example-jars to /usr/share/deployment/classpath/example"
 cp -r /usr/share/example-data/resources/data/employee_file0.avro /data/local-data-store
 cp -r /usr/share/example-data/resources/data/employee_file1.avro /data/local-data-store
