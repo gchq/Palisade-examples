38f722460a5cbaabda0c9ca76c71d65e12f9b868
pd104923 <54805408+pd104923@users.noreply.github.com>
2021-05-05 12:41:50 +0100

    PAL-822 change to jvm startServices script

--- deployment/local-jvm/example-runner/startServices.sh
+++ deployment/local-jvm/example-runner/startServices.sh
@@ -21,10 +21,9 @@ if [ -d $DIR ]; then
   # Important to cd before running the jar - the working directory must be somewhere under Palisade-services
   cd $DIR
   # Start all the services using the service manager from Palisade-services
-  if [ -f $FILE ]; then
+  if [[ -f $FILE ]]; then
     java -jar -Dspring.profiles.active=example-libs,debug $FILE --manager.mode=run
   else
-    echo "${FILE}"
     echo "Cannot find services-manager-<version>-exec.jar - have you run 'mvn install' in Palisade-services?"
   fi
 else
