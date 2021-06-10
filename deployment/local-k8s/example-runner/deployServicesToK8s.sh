1c5a8125e530e0b2d15dd32d213e171c759f10e8
pd104923 <54805408+pd104923@users.noreply.github.com>
2021-06-10 14:35:24 +0100

    PAL-1077 updated image repository property, updated values

--- deployment/local-k8s/example-runner/deployServicesToK8s.sh
+++ deployment/local-k8s/example-runner/deployServicesToK8s.sh
@@ -16,6 +16,7 @@
 cd deployment
 helm dep up
 helm upgrade --install --wait palisade . \
---set global.persistence.dataStores.palisade-data-store.local.hostPath=$(pwd)/resources/data, \
+--set global.hosting=local \
+--set global.persistence.dataStores.palisade-data-store.local.hostPath=$(pwd)/resources/data \
 --set global.persistence.classpathJars.local.hostPath=$(pwd)/deployment/target \
 --set global.deployment=example
