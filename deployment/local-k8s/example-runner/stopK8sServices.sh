abe69e97d1f2305986b0b65590d5581a68b1b045
pd104923 <54805408+pd104923@users.noreply.github.com>
2021-06-11 12:06:07 +0100

    PAL-1077 updated local-k8s deploy script, added local-k8s stop script

---
+++ deployment/local-k8s/example-runner/stopK8sServices.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+# Copyright 2018-2021 Crown Copyright
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+NAMESPACE=$1
+
+cd deployment
+
+if [[ ! -z "$NAMESPACE" ]]
+then
+    echo "Delete all the resources in the $NAMESPACE namespace"
+    # Delete the existing helm deployment
+    helm delete palisade
+
+    # Delete all the resources on the created namespace
+    kubectl delete namespace $NAMESPACE
+else
+    echo "Delete all the resources in the default namespace"
+  # Delete the existing helm deployment
+    helm delete palisade
+
+    # Delete all the resources
+    kubectl delete jobs --all
+    kubectl delete pods --all
+    kubectl delete pvc --all
+fi
