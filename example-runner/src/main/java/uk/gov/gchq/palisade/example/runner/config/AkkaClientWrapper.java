aa4a19cf1e741c33b1c9f5ae24365a4fb2e81e5f
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2021-07-16 13:30:58 +0100

    PAL-0029 updated AkkaClientWrapper

--- example-runner/src/main/java/uk/gov/gchq/palisade/example/runner/config/AkkaClientWrapper.java
+++ example-runner/src/main/java/uk/gov/gchq/palisade/example/runner/config/AkkaClientWrapper.java
@@ -20,8 +20,6 @@ import akka.NotUsed;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import uk.gov.gchq.palisade.client.akka.AkkaClient;
 import uk.gov.gchq.palisade.data.serialise.Serialiser;
@@ -29,33 +27,31 @@ import uk.gov.gchq.palisade.resource.LeafResource;
 import uk.gov.gchq.palisade.util.AbstractResourceBuilder;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Collections;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class AkkaClientWrapper<T> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AkkaClientWrapper.class);
     private final AkkaClient client;
     private final Serialiser<T> serialiser;
-    private final Materializer materializer;
+    private final Materializer materialiser;
 
-    public AkkaClientWrapper(final AkkaClient client, final Serialiser<T> serialiser, final Materializer materializer) {
+    public AkkaClientWrapper(final AkkaClient client, final Serialiser<T> serialiser, final Materializer materialiser) {
         this.client = client;
         this.serialiser = serialiser;
-        this.materializer = materializer;
+        this.materialiser = materialiser;
     }
 
     public <M> Function<Sink<T, M>, M> execute(final String userId, final String fileName, final String purpose) {
         String token = this.register(userId, fileName, purpose).toCompletableFuture().join();
         Source<LeafResource, NotUsed> resources = this.fetch(token);
         Source<T, NotUsed> records = resources.flatMapConcat(resource -> Source.fromJavaStream(() -> this.read(token, resource)));
-        return sink -> records.runWith(sink, materializer);
+        return sink -> records.runWith(sink, materialiser);
     }
 
     public CompletionStage<String> register(final String userId, final String fileName, final String purpose) {
-        return client.register(userId, fileNameToResourceId(fileName), Collections.singletonMap("purpose", purpose));
+        return client.register(userId, AbstractResourceBuilder.create(fileName).getId(), Collections.singletonMap("purpose", purpose));
     }
 
     public Source<LeafResource, NotUsed> fetch(final String token) {
@@ -65,17 +61,5 @@ public class AkkaClientWrapper<T> {
 
     public Stream<T> read(final String token, final LeafResource resource) throws IOException {
         return serialiser.deserialise(client.read(token, resource));
-    }
-
-    private static String fileNameToResourceId(final String fileName) {
-        try {
-            return AbstractResourceBuilder.create(fileName).toString();
-        } catch (IllegalArgumentException ex) {
-            String userDir = System.getProperty("user.dir") + "/";
-            LOGGER.debug("Caught exception while creating URI", ex);
-            LOGGER.debug("Suspect {} is a relative-path so trying with user.dir '{}' property", fileName, userDir);
-            return Path.of(userDir + fileName)
-                    .toUri().normalize().toString();
-        }
     }
 }
